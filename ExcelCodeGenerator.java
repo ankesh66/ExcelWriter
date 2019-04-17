e6a9dce30899243f14af595e4ae8407a24c4fd51
Ankesh Mishra <mishraankesh66@gmail.com>
2019-04-17 14:30:36 +0530

    Update ExcelCodeGenerator.java
    documantation added

--- ExcelCodeGenerator.java
+++ ExcelCodeGenerator.java
@@ -9,7 +9,50 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
+
+
 public class ExcelCodeGenerator {
+	/**
+	
+		if you run code by passing column names as space separated string you will get below result
+
+		Example :
+		columnNames = OrderDate Region Rep Item Units Unit Cost Total
+		delimitor = space
+		output : 
+
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		String sheetName = "";//Write your sheetname here 
+		XSSFSheet excelSheet = workbook.createSheet(sheetName);
+		XSSFRow excelHeader = excelSheet.createRow(0);
+		int count = 0;
+		excelHeader.createCell(count++).setCellValue("OrderDate");
+		excelHeader.createCell(count++).setCellValue("Region");
+		excelHeader.createCell(count++).setCellValue("Rep");
+		excelHeader.createCell(count++).setCellValue("Item");
+		excelHeader.createCell(count++).setCellValue("Units");
+		excelHeader.createCell(count++).setCellValue("Unit");
+		excelHeader.createCell(count++).setCellValue("Cost");
+		excelHeader.createCell(count++).setCellValue("Total");
+
+		int rowIndex = 1
+		for () {//Write your for loop 
+			XSSFRow excelRow = excelSheet.createRow(rowIndex++);
+			count = 0;
+			//Write your logic and computed value here
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+			excelRow.createCell(count++).setCellValue();
+		}
+	
+	**/
+	
+	
 	public void generateExcelWriteCode(String columnNames,String delimitor) {
 		List<String> columns = Arrays.asList(columnNames.split(delimitor));
 		String excelHeaderCode[] = {"XSSFWorkbook workbook = new XSSFWorkbook();\n"+
